9789533c4297aaf0a10d9bf85fea036a38b5f2f6
ABBA3124 <davideabbadessa73@gmail.com>
2024-06-25 13:50:07 +0200

    FINISH

--- src/main/java/davideabbadessa/U2_W2_D2_API_Spring/Controller/AutoreController.java
+++ src/main/java/davideabbadessa/U2_W2_D2_API_Spring/Controller/AutoreController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/autore")
@@ -13,17 +14,49 @@ public class AutoreController {
 
     private List<Autore> autoreList = new ArrayList<>();
 
+    // 1) ritorna la lista di blog post (GET)
     @GetMapping
     public List<Autore> getAllAuthors() {
         return autoreList;
     }
 
+
+    // 2) ritorna un singolo autore (GET)
+    @GetMapping("/{id}")
+    public Autore getAutoreById(@PathVariable int id) {
+        return autoreList.stream().filter(post -> post.getId() == (id)).findFirst().orElse(null);
+    }
+
+    // 3) crea un nuovo blog post (POST)
     @PostMapping
-    public Autore createAuthor(@RequestBody Autore autore) {
+    public Autore createAutore(@RequestBody Autore autore) {
         autore.setId((int) (autoreList.size() + 1));
-        autore.setAvatar("https://ui-avatars.com/api/?name=" + autore.getCognome().replace(" ", "+"));
         autoreList.add(autore);
         return autore;
     }
-    
+
+    // 4) modifica lo specifico blo post (PUT)
+    @PutMapping("/{id}")
+    public Autore updateAutore(@PathVariable int id, @RequestBody Autore modificaAutore) {
+        Optional<Autore> prova = autoreList.stream().filter(post -> post.getId() == (id)).findFirst();
+        if (prova.isPresent()) {
+            Autore autore = prova.get();
+            autore.setNome(modificaAutore.getNome());
+            autore.setCognome(modificaAutore.getCognome());
+            autore.setEmail(modificaAutore.getEmail());
+            autore.setDataDiNascita(modificaAutore.getDataDiNascita());
+            autore.setAvatar(modificaAutore.getAvatar());
+            return autore;
+        } else {
+            return null;
+        }
+    }
+
+    // 5) cancella lo specifico blog post (DELETE)
+    @DeleteMapping("/{id}")
+    public void deleteAutore(@PathVariable int id) {
+        autoreList.removeIf(post -> post.getId() == (id));
+    }
+
+
 }
--- src/main/java/davideabbadessa/U2_W2_D2_API_Spring/Controller/BlogController.java
+++ src/main/java/davideabbadessa/U2_W2_D2_API_Spring/Controller/BlogController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/blog")
@@ -13,16 +14,48 @@ public class BlogController {
 
     private List<Blog> blogList = new ArrayList<>();
 
+
+    // 1) ritorna la lista di blog post
     @GetMapping
     public List<Blog> getBlogList() {
         return blogList;
     }
 
+
+    // 2) ritorna un singolo blog post
+    @GetMapping("/{id}")
+    public Blog getBlogById(@PathVariable int id) {
+        return blogList.stream().filter(post -> post.getId() == (id)).findFirst().orElse(null);
+    }
+
+    // 3) crea un nuovo blog post (POST)
     @PostMapping("/post")
-    public Blog createBlogPost(@RequestBody Blog blog) {
+    public Blog createBlog(@RequestBody Blog blog) {
         blog.setId((int) (blogList.size() + 1));
         blogList.add(blog);
         return blog;
     }
 
+    // 4) modifica lo specifico blo post (PUT)
+    @PutMapping("/{id}")
+    public Blog updateBlog(@PathVariable int id, @RequestBody Blog modificaBlog) {
+        Optional<Blog> prova = blogList.stream().filter(post -> post.getId() == (id)).findFirst();
+        if (prova.isPresent()) {
+            Blog blog = prova.get();
+            blog.setCategoria(modificaBlog.getCategoria());
+            blog.setTitolo(modificaBlog.getTitolo());
+            blog.setCover(modificaBlog.getCover());
+            blog.setContenuto(modificaBlog.getContenuto());
+            blog.setTempoDiLettura(modificaBlog.getTempoDiLettura());
+            return blog;
+        } else {
+            return null;
+        }
+    }
+
+    // 5) cancella lo specifico blog post (DELETE)
+    @DeleteMapping("/{id}")
+    public void deleteBlog(@PathVariable int id) {
+        blogList.removeIf(post -> post.getId() == (id));
+    }
 }
