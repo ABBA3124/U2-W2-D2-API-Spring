ee3cd2840b0f2ee2e20fa3f259d34a49f2b59983
ABBA3124 <davideabbadessa73@gmail.com>
2024-06-25 11:41:23 +0200

    Initial commit

---
+++ src/main/java/davideabbadessa/U2_W2_D2_API_Spring/MyRunner.java
@@ -0,0 +1,4 @@
+package davideabbadessa.U2_W2_D2_API_Spring;
+
+public class MyRunner {
+}
---
+++ src/test/java/davideabbadessa/U2_W2_D2_API_Spring/U2W2D2ApiSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package davideabbadessa.U2_W2_D2_API_Spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class U2W2D2ApiSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
